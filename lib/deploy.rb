a7aab5b65327fd1f5c022c84742f2096297907ab
Michael Smirnoff <michaelsmirnoff@live.com>
2015-12-08 09:44:34 +1030

    Add opinionated config verifier, and app selector.

--- lib/deploy.rb
+++ lib/deploy.rb
@@ -4,6 +4,7 @@ require 'highline'
 require 'rugged'
 require 'aws-sdk'
 require 'deploy/repository'
+require 'deploy/configuration'
 
 module Deploy
   class Deployment
@@ -28,6 +29,29 @@ module Deploy
 
       # Set up AWS params, i.e. region.
       ::Aws.config.update(region: settings['aws_region'])
+
+      # Pull in and verify our deployment configurations
+      configuration = Configuration.new(settings['config_bucket_name'])
+      puts "Checking available configurations... Please wait..."
+      configuration.verify!
+      unless configuration.created_folders.empty?
+        configuration.created_folders.each do |folder|
+          puts "\tCreated #{folder}"
+        end
+      end
+      puts "Check done."
+      # Have the user decide what to deploy
+      apps = configuration.apps
+      app_names = apps.map(&:key)
+      puts "Found applications. Select index of the one to deploy:"
+      longest_key = app_names.max_by(&:length)
+      app_names.each_with_index do |app, index|
+        printf "%-#{longest_key.length}s %s\n", app.sub('/',''), index
+      end
+      app_index = Integer(STDIN.gets)
+      app_bucket = apps.detect { |app| app.key == app_names[app_index] }
+      name = app_bucket.key.sub('/','')
+      puts "App configuration \"#{name}\" selected."
 
     end
 
